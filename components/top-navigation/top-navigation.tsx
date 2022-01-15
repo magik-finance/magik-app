5767647fa0d8e6fa91038d991a789681e9309761
Michal Wrzosek <michal@wrzosek.pl>
2022-01-16 00:11:44 +0100

    feat(top-navigation): desktop version

--- components/top-navigation/top-navigation.tsx
+++ components/top-navigation/top-navigation.tsx
@@ -1,16 +1,16 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
+import Link from 'next/link'
 
 import { LogoIcon } from 'components/icons/logo-icon'
 import { MenuIcon } from 'components/icons/menu-icon'
 import { XMarkIcon } from 'components/icons/x-mark-icon'
-import { NavLink } from 'components/nav-link'
-import NoiseImage from './noise.png'
-import Link from 'next/link'
 import { DiscordIcon } from 'components/icons/discord-icon'
 import { TelegramIcon } from 'components/icons/telegram-icon'
 import { GitHubIcon } from 'components/icons/git-hub-icon'
 import { TwitterIcon } from 'components/icons/twitter-icon'
+import { NavLink } from 'components/nav-link'
+import NoiseImage from './noise.png'
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -19,51 +19,66 @@ const navigation = [
   { name: 'About', href: '#' },
 ]
 
+const icons = [
+  { name: 'Discord', Icon: DiscordIcon, href: '#' },
+  { name: 'Telegram', Icon: TelegramIcon, href: '#' },
+  { name: 'GitHub', Icon: GitHubIcon, href: '#' },
+  { name: 'Twitter', Icon: TwitterIcon, href: '#' },
+]
+
 export const TopNavigation = () => (
   <Popover as="header" className="relative">
     {({ open }) => (
       <>
-        <div className="bg-main-background">
+        <div className="fixed top-0 left-0 z-10 w-full border-b bg-main-background border-b-light-border md:static">
           <nav
-            className="relative flex items-center justify-between h-14 px-6 mx-auto max-w-7xl border-b border-b-light-border sm:px-6"
+            className="relative flex items-center justify-between px-6 mx-auto h-14 max-w-7xl sm:px-6"
             aria-label="Top Navigation"
           >
             <div className="flex items-center flex-1">
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span className="sr-only">Magik</span>
-                  <LogoIcon className="w-auto h-6 sm:h-10" />
+                  <LogoIcon className="w-auto h-6" />
                 </a>
                 <div className="flex items-center -mr-2 md:hidden">
-                  <Popover.Button className="inline-flex items-center justify-center p-2 text-white bg-transparent rounded-md hover:bg-button-hover-background focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
+                  {open ? (
+                    <button className="inline-flex items-center justify-center p-2 text-white bg-transparent rounded-md hover:bg-button-hover-background focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                      <span className="sr-only">Close main menu</span>
                       <XMarkIcon className="w-6 h-6" aria-hidden="true" />
-                    ) : (
+                    </button>
+                  ) : (
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-white bg-transparent rounded-md hover:bg-button-hover-background focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
                       <MenuIcon className="w-6 h-6" aria-hidden="true" />
-                    )}
-                  </Popover.Button>
+                    </Popover.Button>
+                  )}
                 </div>
               </div>
-              <div className="hidden space-x-8 md:flex md:ml-10">
+              <div className="hidden space-x-1 ml-14 md:flex">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="text-base font-medium text-white hover:text-gray-300"
+                    className="px-2 py-3 text-sm font-bold text-white transition-colors hover:text-primary-accent-light"
                   >
                     {item.name}
                   </a>
                 ))}
               </div>
             </div>
-            <div className="hidden md:flex md:items-center md:space-x-6">
-              <a
-                href="#"
-                className="inline-flex items-center px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md hover:bg-gray-700"
-              >
-                Connect wallet
-              </a>
+            <div className="hidden md:flex md:items-center md:space-x-1">
+              {icons.map(({ name, Icon, href }) => (
+                <Link key={name} href={href} passHref={true}>
+                  <a
+                    title={name}
+                    target="_blank"
+                    className="block px-1 py-2 text-white transition-colors hover:text-primary-accent-light"
+                  >
+                    <Icon className="w-5 h-5" />
+                  </a>
+                </Link>
+              ))}
             </div>
           </nav>
         </div>
@@ -80,9 +95,9 @@ export const TopNavigation = () => (
           <Popover.Panel
             focus
             style={{ height: 4 * 110 }}
-            className="absolute inset-x-0 top-14 transition origin-top transform overflow-hidden bg-main-background md:hidden"
+            className="fixed inset-x-0 z-10 mt-px overflow-hidden transition origin-top transform border-b top-14 bg-main-background border-b-light-border md:hidden"
           >
-            <div className="bg-primary-accent -top-16 -right-4 absolute w-36 h-36 opacity-50 blur-3xl rotate-45" />
+            <div className="absolute rotate-45 opacity-50 bg-primary-accent -top-16 -right-4 w-36 h-36 blur-3xl" />
             <div
               className="absolute inset-0 bg-repeat"
               style={{
@@ -99,33 +114,24 @@ export const TopNavigation = () => (
                     activeClassName="text-white"
                     inactiveClassName="text-soft-font"
                   >
-                    <a className="block px-3 py-2 text-2xl font-medium rounded-md hover:text-white">
+                    <a className="block px-3 py-2 text-2xl font-medium transition-colors rounded-md hover:text-white">
                       {item.name}
                     </a>
                   </NavLink>
                 ))}
               </div>
               <div className="flex mt-8">
-                <Link href="#" passHref={true}>
-                  <a className="block px-3 py-2 text-soft-font hover:text-white">
-                    <DiscordIcon className="w-5 h-5" />
-                  </a>
-                </Link>
-                <Link href="#" passHref={true}>
-                  <a className="block px-3 py-2 text-soft-font hover:text-white">
-                    <TelegramIcon className="w-5 h-5" />
-                  </a>
-                </Link>
-                <Link href="#" passHref={true}>
-                  <a className="block px-3 py-2 text-soft-font hover:text-white">
-                    <GitHubIcon className="w-5 h-5" />
-                  </a>
-                </Link>
-                <Link href="#" passHref={true}>
-                  <a className="block px-3 py-2 text-soft-font hover:text-white">
-                    <TwitterIcon className="w-5 h-5" />
-                  </a>
-                </Link>
+                {icons.map(({ name, Icon, href }) => (
+                  <Link key={name} href={href} passHref={true}>
+                    <a
+                      title={name}
+                      target="_blank"
+                      className="block px-3 py-2 transition-colors text-soft-font hover:text-white"
+                    >
+                      <Icon className="w-5 h-5" />
+                    </a>
+                  </Link>
+                ))}
               </div>
             </div>
           </Popover.Panel>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'light-border': '#333744',
         'soft-font': '#575B6A',
         'primary-accent': '#4E27B2',
+        'primary-accent-light': '#935edf',
       },
       fontFamily: {
         sans: ['Gilmer', 'Helvetica', 'Arial', 'sans-serif'],
