5393a8e547495a3ef2478e60217c4b2989ee8528
Michal Wrzosek <michal@wrzosek.pl>
2022-01-16 22:01:40 +0100

    feat(hero-header): desktop sign up form spacing tweaks

--- components/sign-up-for-beta-form/sign-up-for-beta-form.tsx
+++ components/sign-up-for-beta-form/sign-up-for-beta-form.tsx
@@ -69,14 +69,14 @@ export const SignUpForBetaForm: VFC<{ className?: string }> = ({
           type="email"
           placeholder="Example@gmail.com"
           className={clsx(
-            'px-6 py-3 md:px-8 md:py-4 mt-3 text-sm md:text-lg font-medium text-white border-none rounded-lg placeholder:text-white bg-gradient-to-r from-gray-800 to-gray-900',
+            'px-6 py-3 md:px-8 md:py-4 mt-3 md:mt-4 text-sm md:text-lg font-medium text-white border-none rounded-lg placeholder:text-white bg-gradient-to-r from-gray-800 to-gray-900',
             {
               'ring-2 ring-red-600 focus:ring-2 focus:ring-red-700':
                 !!errors.email,
             }
           )}
         />
-        <div className="relative mt-3">
+        <div className="relative mt-3 md:mt-4">
           <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900" />
           <select
             {...register('lead', {
@@ -102,14 +102,17 @@ export const SignUpForBetaForm: VFC<{ className?: string }> = ({
             ))}
           </select>
         </div>
-        <button type="submit" className="relative w-full mt-3 rounded-lg group">
+        <button
+          type="submit"
+          className="relative w-full mt-3 rounded-lg md:mt-4 group"
+        >
           <ColorfulLayer className="transition-opacity opacity-0 blur-md rounded-2xl group-hover:opacity-60 group-focus:opacity-60" />
           <div className="absolute inset-0 rounded-lg bg-primary-accent" />
           <div className="relative px-6 py-3 text-sm font-medium text-center md:px-10 md:py-6 md:text-xl">
             Send your details for the Beta
           </div>
         </button>
-        <div className="flex w-full gap-2 mt-6">
+        <div className="flex w-full gap-2 mt-6 md:mt-7">
           <UsersIcon className="w-6 h-6 shrink-0" />
           <div className="text-xs font-medium leading-relaxed text-soft-font">
             <span className="hidden md:inline">
