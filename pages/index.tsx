09c4bd33a1e1737e654eac009282b7958758cf6b
Michal Wrzosek <michal@wrzosek.pl>
2022-02-02 16:09:16 +0100

    meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import { CookieBanner } from 'components/CookieBanner'
 import { Footer } from 'components/Footer'
 import { HeroHeader } from 'components/HeroHeader'
@@ -6,14 +8,19 @@ import { TopNavigation } from 'components/TopNavigation'
 
 export default function Index() {
   return (
-    <div className="relative overflow-hidden bg-main-background">
-      <TopNavigation />
-      <main>
-        <HeroHeader />
-        <InfoSection />
-      </main>
-      <Footer />
-      <CookieBanner />
-    </div>
+    <>
+      <Head>
+        <title>Magik</title>
+      </Head>
+      <div className="relative overflow-hidden bg-main-background">
+        <TopNavigation />
+        <main>
+          <HeroHeader />
+          <InfoSection />
+        </main>
+        <Footer />
+        <CookieBanner />
+      </div>
+    </>
   )
 }
